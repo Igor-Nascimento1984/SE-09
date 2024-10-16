e14e018b3fb69988f867240b3038447a18ef3de8
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-16 00:14:46 -0300

    Final da aula76

--- src/aplication01/Program.java
+++ src/aplication01/Program.java
@@ -1,9 +1,38 @@
 package aplication01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner edd = new Scanner(System.in);
+
+		System.out.println("DADOS DO PRODUTO");
+		System.out.print("PRODUTO:");
+		String nome = edd.nextLine();
+		System.out.print("PREÇO:");
+		double preco = edd.nextDouble();
+		System.out.print("QUANTIDADE:");
+		int quantidade = edd.nextInt();
+
+		Product produto = new Product(nome, preco, quantidade);
 		
+		System.out.println(produto.toString());
+
+		System.out.print("ADICONAR PRODUTO: ");
+		quantidade = edd.nextInt();
+		produto.addProduto(quantidade);
+		System.out.println(produto.toString());
+		System.out.print("RETIRAR PRODUTO: ");
+		quantidade = edd.nextInt();
+		produto.removeProduto(quantidade);
+		System.out.println(produto.toString());
+		edd.close();
 
 	}
 
