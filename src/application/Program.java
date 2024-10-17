074f2669bd0cb51adb991b2e64f0c846654bdf4e
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-17 02:15:47 -0300

    Final da aula82ex01

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,69 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entittes01.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
 
+		Locale.setDefault(Locale.US);
+		Scanner edd = new Scanner(System.in);
+
+		System.out.print("NUMERO CONTA: ");
+		int numeroConta = edd.nextInt();
+		System.out.print("TITULAR:");
+		String nome = edd.next();
+		// edd.nextLine();
+		System.out.print("DEPOSITO INICIAL: S/N" );
+		String escolha = edd.next();
+		edd.nextLine();
+
+		if (escolha.charAt(0) == 'S') {
+			System.out.print("DEPOSITO INICIAL");
+			double saldo = edd.nextDouble();
+
+			Account conta = new Account(numeroConta, nome, saldo);
+
+			System.out.println("CONTA: " + conta.getNumeroConta() + " - TITULAR: " + conta.getNome() + " - SALDO: R$"
+					+ conta.getSaldo());
+			
+			System.out.print("ADIONAR VALOR: R$");
+			double deposito = edd.nextDouble();
+			conta.deposito(deposito);
+			
+			System.out.println("CONTA: " + conta.getNumeroConta() + " - TITULAR: " + conta.getNome() + " - SALDO: R$"
+					+ conta.getSaldo());
+			
+			System.out.print("SACAR VALOR: R$");
+			double saque = edd.nextDouble();
+			conta.saque(saque);
+			
+			System.out.println("CONTA: " + conta.getNumeroConta() + " - TITULAR: " + conta.getNome() + " - SALDO: R$"
+					+ conta.getSaldo());
+			
+		} else {
+			Account conta = new Account(numeroConta, nome);
+
+			System.out.println("CONTA: " + conta.getNumeroConta() + " - TITULAR: " + conta.getNome() + " - SALDO: R$"
+					+ conta.getSaldo());
+			
+			System.out.print("ADIONAR VALOR: R$");
+			double deposito = edd.nextDouble();
+			conta.deposito(deposito);
+			
+			System.out.println("CONTA: " + conta.getNumeroConta() + " - TITULAR: " + conta.getNome() + " - SALDO: R$"
+					+ conta.getSaldo());
+			
+			System.out.print("SACAR VALOR: R$");
+			double saque = edd.nextDouble();
+			conta.saque(saque);
+			
+			System.out.println("CONTA: " + conta.getNumeroConta() + " - TITULAR: " + conta.getNome() + " - SALDO: R$"
+					+ conta.getSaldo());
+		}
+		edd.close();
 	}
-
 }
--- src/entittes01/Account.java
+++ src/entittes01/Account.java
@@ -1,5 +1,61 @@
 package entittes01;
 
 public class Account {
+		
+	private int numeroConta;
+	private String nome;
+	private double saldo;
+	
+	public Account() {
+		}
+	
+	public Account(int numeroConta, String nome) {
+		this.numeroConta = numeroConta;
+		this.nome = nome;
+		this.saldo = 0;
+	}
 
+	public Account(int numeroConta, String nome, double saldo) {
+		this.numeroConta = numeroConta;
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void deposito(double saldo) {
+		this.saldo += saldo;
+	}
+	
+	public void saque(double saldo) {
+		this.saldo -= (saldo + 5);
+	}
+	
+	/*public String toString() {
+		return "CONTA: "
+				+ numeroConta
+				+" - TITULAR: "
+				+ nome
+				+" - SALDO: R$"
+				+saldo;
+	}*/
+	 
 }
